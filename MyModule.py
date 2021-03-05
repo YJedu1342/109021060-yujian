8e99f44c66dbca59d7ddfd6da54d5a2675894459
YJedu1342 <109021060@live.asia.edu.tw>
2021-03-05 14:55:44 +0800

    20210305

--- MyModule.py
+++ MyModule.py
@@ -1,8 +1,8 @@
 def plus(a,b):
     return a+b
-def less(a.b):
+def less(a,b):
     return a-b
-def multiply(a.b):
+def multiply(a,b):
     return a*b
-def division(a.b):
+def division(a,b):
     return a/b
