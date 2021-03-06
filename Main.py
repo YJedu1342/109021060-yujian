5b95efc3dfc1f3adf0b54b665537cd8b25223576
YJedu1342 <109021060@live.asia.edu.tw>
2021-03-06 10:55:53 +0800

    20210306 YJ

--- Main.py
+++ Main.py
@@ -1,2 +1,5 @@
-#import MyModule as fun
-print("1")
+import sys
+from MyModule import getsquare
+#當前只保留陣列平方 目前程式有:費波那契數列、陣列平方、組合數C、求數值位數、求證是否為回文
+x=list(map(int,sys.argv[1].split()))
+print(getsquare(x))
--- MyModule.py
+++ MyModule.py
@@ -1,13 +1,43 @@
-def plus(a,b):
+def getplus(a,b):
     return a+b
-def less(a,b):
+def getless(a,b):
     return a-b
-def multiply(a,b):
+def getmultiply(a,b):
     return a*b
-def division(a,b):
+def getdivision(a,b):
     return a/b
-def fs(a):
+def getFibonacci(a):
     if a<=1:
         return 1
     else:
-        return fs(a-1)+fs(a-2)
+        return getFibonacci(a-1)+getFibonacci(a-2)
+def getcombination(a,b):
+    tmp1=1
+    tmp2=1
+    tmp3=1
+    for i in range(1,a+1):
+        tmp1*=i
+    for i in range(1,b+1):
+        tmp2*=i
+    for i in range(1,a-b+1):
+        tmp3*=i
+    return tmp1/(tmp2*tmp3)
+def getsquare(a):
+    for i in range(len(a)):
+        a[i]=a[i]*a[i]
+    return a
+def getnumberdigit(a):    
+    i=0
+    while a>0:
+        i+=1
+        a=a//10
+    return i
+def getpalindrome(a):
+    flag=True
+    tmp=len(a)//2
+    for i in range(tmp):
+        if a[i]==a[len(a)-1-i]:
+            flag=True
+        else:
+            flag=False
+    return flag
